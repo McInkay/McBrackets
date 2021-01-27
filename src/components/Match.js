a9795eee4a7d15227091d0046b506f12d677f245
Al McKinlay <github@10people.co.uk>
2021-01-27 22:08:03 +0000

    Align all winners (Except final) in the middle of the 2 others

--- src/components/Match.js
+++ src/components/Match.js
@@ -2,10 +2,9 @@ import Team from "./Team";
 import tw from "tailwind-styled-components";
 
 const Container = tw.div`
-    flex
-    items-start
-    justify-start
-    flex-col
+    grid
+    grid-cols-1
+    content-around
     w-full
     ${({level, roundorder, total}) => `col-start-${roundorder < total / 2 ? 1 + level : 9 - level}`}
     ${({level, roundorder, total}) => `row-start-${roundorder < total / 2 ? 1 + ((level > 0 ? level * 2 : 1) * roundorder) : 1 + ((level > 0 ? level * 2 : 1) * (roundorder - (total / 2)))}`}
