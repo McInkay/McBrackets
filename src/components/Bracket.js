238e1a08b7b2dd777ccab626f1869b638f31fad4
Al McKinlay <github@10people.co.uk>
2021-02-06 20:20:54 +0000

    Fix issue with the semi-final/final filling

--- src/components/Bracket.js
+++ src/components/Bracket.js
@@ -3,13 +3,27 @@ import RoundPart from "./RoundPart";
 
 const space = "\u00a0";
 
+const setTeamFunc = (bracket, setBracket) => (round, match, team) => {
+  const newBracket = JSON.parse(JSON.stringify(bracket));
+  if (round < bracket.length - 3) {
+    // Normal round
+    newBracket[round + 1][Math.floor(match / 2)][match % 2] = team;
+  } else if (round === bracket.length - 3) {
+    // Clicking on a semi-finalist
+    newBracket[round + 1][match % 2][0] = team;
+  } else if (round === bracket.length - 2) {
+    // Clicking on a finalist
+    newBracket[round + 1][0][0] = team;
+  } else {
+    // We have clicked on the winner
+    return;
+  }
+  setBracket(newBracket);
+}
+
 function BracketView() {
   const [bracket, setBracket] = useState([]);
-  const setTeam = (round, match, team) => {
-    const newBracket = JSON.parse(JSON.stringify(bracket));
-    newBracket[round + 1][Math.floor(match / 2)][match % 2] = team;
-    setBracket(newBracket);
-  }
+  const setTeam = setTeamFunc(bracket, setBracket);
   if (bracket.length === 0) {
     const startingRound = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12], [13, 14], [15, 16], [17, 18], [19, 20], [21, 22], [23, 24], [25, 26], [27, 28], [29, 30], [31, 32]];
     const newBracket = [startingRound];
