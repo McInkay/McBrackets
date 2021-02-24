3bf441cdd676b86f8f472e1e8f4377c87a969dbe
Al McKinlay <github@10people.co.uk>
2021-02-24 20:43:23 +0000

    Add slack madness logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Bracket from "./components/Bracket";
 import "tailwindcss/tailwind.css";
+import logo from './logo.png';
+import tw from "tailwind-styled-components";
+
+const Logo = tw.img`
+  w-80
+  m-auto
+`;
 
 function App() {
   return (
     <div className="App">
       <nav className="py-4 px-6 border-solid border-b-2 border-gray-500">
-        <span className="font-semibold text-xl tracking-tight">McBrackets</span>
+        <Logo src={logo} alt="logo" />
       </nav>
     
       <main className="pt-5">
